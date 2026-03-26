6aa59665e24c3c1edd888e2f7b1c7804ad331de3
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-26 15:52:28 -0400

    fix: add necessary imports to app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 
 import { Component } from '@angular/core';
-import { IonApp, IonSplitPane, IonRouterOutlet, IonNote, IonList, IonListHeader } from '@ionic/angular/standalone';
+import { IonApp, IonSplitPane, IonRouterOutlet, IonNote, IonList, IonListHeader, IonMenu, IonContent } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
 
@@ -8,7 +8,7 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [ IonApp, IonSplitPane, IonRouterOutlet, IonNote, IonList, IonListHeader],
+  imports: [ IonApp, IonSplitPane, IonRouterOutlet, IonNote, IonList, IonListHeader, IonMenu, IonContent],
 })
 export class AppComponent {
   constructor() {
