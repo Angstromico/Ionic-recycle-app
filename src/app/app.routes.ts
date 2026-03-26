f3de6fc781da4cebefc290e602be0dd7f5e3409b
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-26 16:56:12 -0400

    feat: register page backbone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
 
 
 ];
