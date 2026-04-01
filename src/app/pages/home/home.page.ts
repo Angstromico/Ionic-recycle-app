aa31601a8e32b4c8e92d0a7a7a8d28ee0dea5d64
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-01 16:25:46 -0400

    feat: put contributors in home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonLabel,
+} from '@ionic/angular/standalone';
+
+interface IContributors {
+  id: string;
+  name: string;
+  image?: string;
+  contributions: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonList,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonMenuButton,
+    IonItem,
+    IonAvatar,
+    IonLabel,
+  ],
 })
 export class HomePage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  defaultImg =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5jTKo4WuNTHYrShmskUJXipMid-jcnrAfAQ&s';
 
+  contributors = signal<IContributors[]>([
+    {
+      id: '1',
+      name: 'Manuel Morales',
+      contributions: 100,
+    },
+    {
+      id: '2',
+      name: 'John Doe',
+      contributions: 50,
+    },
+    {
+      id: '3',
+      name: 'Jane Smith',
+      contributions: 75,
+    },
+  ]);
 }
